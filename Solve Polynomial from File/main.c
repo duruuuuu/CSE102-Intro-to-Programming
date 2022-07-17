61a0467260d0ff593ab3c25d4aac3b988457e98c
duruuuuu <105268208+duruuuuu@users.noreply.github.com>
2022-07-17 21:32:56 +0300

    Update main.c

--- Solve Polynomial from File/main.c
+++ Solve Polynomial from File/main.c
@@ -3,8 +3,6 @@
 
 int main()
 {
-    printf("\n////////////////////////////\n");
-    int x = 5;
     FILE *val;
     FILE *polynomial;
     FILE *eval;
@@ -33,7 +31,6 @@ int main()
     if(poly[0] != '-')
         count++;
     int count2 = count-1; int z=0;
-    //printf("expression count = %d ; count2 = %d\n", count, count2);
     char tempC[100], tempP[100];
     double coeff, power, coefficients[100], powers[100];
     int countParse=0, pow_index, x_index;
@@ -43,29 +40,18 @@ int main()
         if(poly[i] == '+' || poly[i] == '-' || i == 0)
         {
             int subIndex = i;
-            //printf("\n\n %d = %c\n", subIndex, poly[subIndex]);
            
             for(int j=0; j<=countParse; j++, subIndex++)
             {
-                //printf("hello%d: %c\n", subIndex, poly[subIndex]);
 
                 if(poly[subIndex] == 'x')
-                {
                     x_index = subIndex;
-                    //printf("x: %d\n", x_index);
-                }
 
                 if(poly[subIndex] == '^')
-                {
                     pow_index = subIndex;
-                    //printf("pow: %d\n", pow_index);
-                }
             }
-            //printf("checkpoint\n");
             int lastIndex = subIndex;
-            //printf("last index: %d\n", lastIndex);
             subIndex = i;
-            //printf("sub index: %d\n", subIndex);
             for(subIndex=i; subIndex<lastIndex;subIndex++)//going through coefficients
             {
                 if(poly[x_index-1] == '+' || poly[x_index-1] == '-')
@@ -82,13 +68,11 @@ int main()
                     if(subIndex < x_index)
                     {
                         tempC[z] = poly[subIndex];
-                        //printf("%d : tempC: %c\n", z, tempC[z]);
                         z++;
                     }
                 }
             }
             z=0;
-            //printf("\ncheckpoint\n");
             sscanf(tempC, "%lf", &coeff);
             coefficients[count2] = coeff;
             
@@ -98,8 +82,7 @@ int main()
                 {
                     if(poly[x_index+1] == '^')
                         tempP[z] = poly[subIndex];
-                        //printf("%d : tempP : %c\n", z, tempP[z]);
-                        z++;
+                    z++;
                 }
             }
             z=0;
@@ -110,14 +93,9 @@ int main()
             }
             else if(poly[x_index+1] != '^')
                 powers[count2] = 1;
-
-            //printf("coeff : %d : %.2lf\n", count2, coefficients[count2]);
-            //printf("pow : %d : %.1lf\n", count2, powers[count2]);
-            
             
             count2--; //index for coefficient and power
             countParse = 0; //resetting
-            //printf("\n");
 
             for(int r=0; r<100; r++)//emptying temp arrays
             {
@@ -129,57 +107,21 @@ int main()
     }
 
     double ans=0, value;
-    /*for(int r=0; r<count; r++)
-    {
-        printf("%d : %.1lf : %.1lf\n", r,coefficients[r], powers[r]);
-    }*/
     printf("\n");
     for(int h=0; h<polyIndex;h++)
-    {
         printf("%c", poly[h]);
-    }
-printf("\n\n");
+    printf("\n\n");
     while(fscanf(val, "%lf", &value) != EOF)
     {
-        //printf("value = %lf\n", value);
         for(int r=0; r<count; r++)
             ans += (pow(value, powers[r]))*(coefficients[r]);
         printf("x = %lf [ans = %.2lf]\n", value, ans);
         fprintf(eval, "%.2lf\n", ans);
         ans = 0;
     }
-    
 
     fclose(val);
     fclose(polynomial);
     fclose(eval);
-    
-    printf("\n////////////////////////////\n");
     return 0;
 }
-/*
-    if there is no ^ after the x, poewr is 1. if there is a ^ after the x, get the power
-    if there is no coefficient before x, coefficient is 1. if there is, get coeffieciednt
-
-    arrays:
-    coefficient, power
-
-    for(int i=0; i<power/coeffIndex; i++)
-    {
-        ans = pow(power[i], x);
-        ans = ans*coefficient;
-    }
-    
-    coefficients
-    0: 5
-    1: 23.5
-    2: 1
-    
-    powers
-    0: 1
-    1: 3
-    2: 2
-
-    ans = pow(powers[i], x);
-    ans = coefficients[i]*ans;
-*/
